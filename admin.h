28787ada42b74ab11fa5bfe509ef33f327f3b6a2
Bry4L <bljx0515@gmail.com>
2025-01-16 23:29:57 +0800

    Add files via upload

---
+++ admin.h
@@ -0,0 +1,10 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include "authentication.h"
+#include "student.h"
+#include "instructor.h"
+
+void adminMenu(UserList& userList, StudentList& studentList, InstructorList& instructorList); // reference to the object contain list of all data //allow to perform
+
+#endif
