a00ebbe1db665fd35a1e5037e50734adcd5f5e51
Bry4L <bljx0515@gmail.com>
2025-01-19 13:41:11 +0800

    Update admin.h

--- Headers/admin.h
+++ Headers/admin.h
@@ -4,7 +4,8 @@
 #include "authentication.h"
 #include "student.h"
 #include "instructor.h"
+#include <vector>
 
-void adminMenu(UserList& userList, StudentList& studentList, InstructorList& instructorList); // reference to the object contain list of all data //allow to perform
+void adminMenu(UserList& userList, vector<Student>& students, InstructorList& instructorList); // reference to the object contain list of all data //allow to perform
 
 #endif
