a37ed9cbf192d168f4698fd981b70c40e827cb92
Bry4L <bljx0515@gmail.com>
2025-01-22 01:58:07 +0800

    Generate Report function implement

--- Headers/admin.h
+++ Headers/admin.h
@@ -7,5 +7,8 @@
 #include <vector>
 
 void adminMenu(UserList& userList, vector<Student>& students, InstructorList& instructorList); // reference to the object contain list of all data //allow to perform
+void generateEnrolledStudentsReport(const CourseManagement& courseManagement);
+void generateAvailableCoursesReport(const CourseManagement& courseManagement);
+
 
 #endif
